0206842793827df1ca103254af0303661d443587
Laura Micheller Peterson <michellepeterson.mishmish@gmail.com>
2021-01-24 08:06:02 -0700

    initial commit with file setup and reference code

---
+++ app.js
@@ -0,0 +1,19 @@
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw new Error(err);
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
